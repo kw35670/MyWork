b9fb234e7cdc9ba0f0254631ac9ef0b588e5f2e0
riku <tkyu58690@gmail.com>
2022-11-12 22:52:56 +0900

    added vanta.js

---
+++ js/script.js
@@ -0,0 +1,13 @@
+VANTA.BIRDS({
+    el: "#bird",
+    mouseControls: true,
+    touchControls: true,
+    gyroControls: false,
+    minHeight: 200.0,
+    minWidth: 200.0,
+    scale: 1.0,
+    scaleMobile: 1.0,
+    backgroundColor: 0xffffff,
+    quantity: 4.00,
+    birdSize: 1
+});
